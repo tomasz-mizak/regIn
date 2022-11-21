18432a36ab904ac4c603ed5c1040f4b9d49f4704
Toma <tomasz.mizak99@gmail.com>
2022-11-21 11:34:52 +0100

    update

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
+from pprint import pprint
 from cx_Oracle import init_oracle_client
 from sqlalchemy import create_engine, text
+import pandas as pd
 
 # import Config class from config.py - enviroment variables helper
 from config import Config
 conf = Config()
 
+# initial settins
+DEBUG = conf.get('DEBUG')
+
 # initialize oracle client
-init_oracle_client(conf.get('INSTANT_CLIENT_PATH'))
+# init_oracle_client(conf.get('INSTANT_CLIENT_PATH')) ? need this?
 
 # connection string - as easy to read format
 # .env keys USERNAME, PASSWORD, DATABASE_IP, DATABASE_PORT, DATABASE_NAME
-connection_string = "oracle+cx_oracle://%s:%s@%s:%s/?service_name=%s&encoding=UTF-8&nencoding=UTF-8"
-connection_string.format(
+connection_string = "oracle+cx_oracle://%s:%s@%s:%s/?service_name=%s&encoding=UTF-8&nencoding=UTF-8" % (
     conf.get('USERNAME'), 
     conf.get('PASSWORD'), 
     conf.get('DATABASE_IP'), 
     conf.get('DATABASE_PORT'), 
     conf.get('DATABASE_NAME'))
 
+# Print connection string if debug settings is enabled
+if DEBUG:
+    print(connection_string)
+
 # define engine for connection
 engine = create_engine(connection_string)
+
+# TODO: handle timeout and other erros and send log to zi@wpia.uni.lodz.pl
 
 # prepare connection
 with engine.connect() as conn:
     # read sql from file to prepare as query
     with open('script.sql') as f:
         query = text(f.read())
-        conn.execute(query)
-        # TODO: Check execute result and insert + commit this result to database.
-
+        r = pd.read_sql(query, con=conn)
+        print(f'Inserts todo: {r.shape[0]}')
+        for i, r in r.iterrows():
+            print(f"{i+1}) ----------------")
+            ii = r[0].replace(';', '') # insert instruction
+            if DEBUG:
+                print(ii)
+            try:
+                conn.execute(r[0])
+                print("success!")
+            except Exception as ex:
+                if (DEBUG):
+                    print(ex.args)
